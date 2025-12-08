4ae25548bb0073c804dca6ba6688e945c3564645
Cicero Bugash <cicero.bugash@gmail.com>
2025-12-08 05:35:23 -0500

    auto

--- iterate.sh
+++ iterate.sh
@@ -2,21 +2,21 @@ echo "Building project in release mode..."
 RUSTFLAGS="-A warnings" cargo build --release
 echo "--------------------------------------------------"
 
-echo "Running rust_smoke_test_ffi..."
-RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_test_ffi
-echo "--------------------------------------------------"
+# echo "Running rust_smoke_test_ffi..."
+# RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_test_ffi
+# echo "--------------------------------------------------"
 
-echo "Running rust_smoke_replay_full..."
-RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_replay_full
-echo "--------------------------------------------------"
+# echo "Running rust_smoke_replay_full..."
+# RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_replay_full
+# echo "--------------------------------------------------"
 
-echo "Running rust_smoke_replay_full_cuda..."
-RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_replay_full_cuda
-echo "--------------------------------------------------"
+# echo "Running rust_smoke_replay_full_cuda..."
+# RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_replay_full_cuda
+# echo "--------------------------------------------------"
 
-echo "Running rust_smoke_checkpoint_roundtrip..."
-RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_checkpoint_roundtrip
-echo "--------------------------------------------------"
+# echo "Running rust_smoke_checkpoint_roundtrip..."
+# RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo run --example rust_smoke_checkpoint_roundtrip
+# echo "--------------------------------------------------"
 
 echo "Running rust_smoke_checkpoint_roundtrip..."
 RUST_BACKTRACE=full RUSTFLAGS="-A warnings" cargo test --example rust_comprehensive_internal_tests
