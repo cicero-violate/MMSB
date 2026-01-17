d0ae071fba930c127f5138bbea5d0252c1f9ad35
Cicero Bugash <cicero.bugash@gmail.com>
2026-01-17 08:56:54 -0500

    migrate: page and delta core (page.rs, delta.rs, delta_validation)

--- mmsb-memory/src/delta/mod.rs
+++ mmsb-memory/src/delta/mod.rs
@@ -1,3 +1,8 @@
 pub mod delta_types;
 
+pub mod delta;
+pub mod delta_validation;
+
 pub use delta_types::{DeltaError, DeltaID, Source};
+pub use delta::Delta;
+pub use delta_validation::validate_delta;
--- mmsb-memory/src/page/mod.rs
+++ mmsb-memory/src/page/mod.rs
@@ -1,5 +1,7 @@
 pub mod gc;
 pub mod page_types;
+pub mod page;
 
 pub use gc::{GCMetrics, MemoryPressureHandler};
 pub use page_types::{PageError, PageID, PageLocation};
+pub use page::{Page, Metadata};
