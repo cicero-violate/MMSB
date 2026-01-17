6b8f77df6de44f966f68c728f8b288efe13e0aff
Cicero Bugash <cicero.bugash@gmail.com>
2026-01-17 12:00:47 -0500

    auto

--- mmsb-memory/src/commit/commit_delta.rs
+++ mmsb-memory/src/commit/commit_delta.rs
@@ -2,12 +2,13 @@ use crate::delta::Delta;
 use crate::dag::DependencyGraph;
 use crate::tlog::TransactionLog;
 use mmsb_proof::AdmissionProof;  // Canonical from mmsb-proof
+
 pub fn commit_delta(
-log: &TransactionLog,
-admission: &AdmissionProof,
-delta: &Delta,
-dag: Option<&DependencyGraph>,
+    log: &TransactionLog,
+    admission: &AdmissionProof,
+    delta: &Delta,
+    dag: Option<&DependencyGraph>,
 ) -> Result<(), std::io::Error> {
-// Stub implementation (expand as needed)
-Ok(())
+    // Stub implementation (expand as needed)
+    log.append(admission, delta.clone())
 }
