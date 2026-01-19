5fb5be0ff6441dfae2038ed2bec515df7f484f10
Cicero Bugash <cicero.bugash@gmail.com>
2026-01-18 19:40:32 -0500

    auto

--- mmsb-memory/src/delta/mod.rs
+++ mmsb-memory/src/delta/mod.rs
@@ -5,7 +5,7 @@ pub mod delta_validation;
 pub mod delta_merge;
 pub mod columnar_delta;
 
-pub use delta_types::{DeltaError, DeltaID, Source};
+pub use delta_types::{DeltaError, Source};
 pub use delta::Delta;
 pub use delta_validation::validate_delta;
 pub use delta_merge::merge_deltas;
