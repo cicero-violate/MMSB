dccd644bfbb019678986dfbae769d8d6ef1af8c4
Cicero Bugash <cicero.bugash@gmail.com>
2026-01-20 18:53:43 -0500

    auto

--- mmsb-service/src/lib.rs
+++ mmsb-service/src/lib.rs
@@ -1,4 +1,3 @@
-// src/lib.rs
 //! MMSB Service Runtime
 //!
 //! Process-local wiring only.
@@ -7,20 +6,22 @@
 //! All correctness flows through MMSB memory.
 
 mod runtime;
-pub use runtime::Runtime;
 mod protocol_signal_out;
 mod protocol_signal_in;
 mod scheduler;
+mod runtime_context;
+mod service;
+mod service_context;
+mod wake_source;
+
+pub mod services;
+
+pub use runtime::Runtime;
+pub use runtime_context::RuntimeContext;
 
 pub use protocol_signal_out::ProtocolSignalOut;
 pub use protocol_signal_in::ProtocolSignalIn;
 pub use scheduler::Scheduler;
-
-// src/lib.rs (additions)
-mod service;
-mod service_context;
-mod wake_source;
-pub mod services;
 
 pub use service::Service;
 pub use service_context::ServiceContext;
