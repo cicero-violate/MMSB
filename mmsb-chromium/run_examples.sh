3540f8a216832020065a9af8df88526bf8c71733
Cicero Bugash <cicero.bugash@gmail.com>
2026-01-20 22:44:44 -0500

    auto

--- mmsb-chromium/run_examples.sh
+++ mmsb-chromium/run_examples.sh
@@ -6,6 +6,6 @@ cargo build
 # cargo run --example create_intent
 # cargo run --example judgment_pipeline
 # cargo run --example mmsb_service_example
-cargo run --example test_chatgpt_roundtrip
-
+# cargo run --example test_chatgpt_roundtrip
+cargo run --example test_chatgpt_roundtrip -- "ping"
 
